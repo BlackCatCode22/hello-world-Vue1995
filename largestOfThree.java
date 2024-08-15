0238762dd6087377b7340501b20b74a21ec97980
Vue1995 <Sheenavue1995@yahoo.com>
2024-08-15 12:13:32 -0700

    Add files via upload

---
+++ largestOfThree.java
@@ -0,0 +1,83 @@
+//This is a comment.
+/*
+ **Sheena Vue
+ * 15Aug2024
+ * LargestOfThree.java
+ * /
+ */
+import java.util.Scanner; // Import the Scanner class
+
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        System.out.println("\n\n Welcome to the Double Int Program! \n\n");
+
+        int firstNum = 0;
+        int secondNum = 0;
+        int thirdNum = 0;
+
+        // Create a Scanner object
+        Scanner myScannerObject = new Scanner(System.in);
+
+        // Ask user for the first int
+        System.out.println("\n Please enter a whole number: ");
+        firstNum = myScannerObject.nextInt();
+
+        // Unit text for our first user input.
+        System.out.println("\n The value of firstNum is: " + firstNum);
+
+        // Ask user for the second int
+        System.out.println("\n Please enter a whole number: ");
+        secondNum = myScannerObject.nextInt();
+
+        // Unit text for our second user input.
+        System.out.println("\n The value of secondNum is : " + secondNum);
+
+        // Ask user for the third int
+        System.out.println("\n Please enter a third whole number: ");
+        secondNum = myScannerObject.nextInt();
+
+        // Unit text for our third user input.
+        System.out.println("\n The value of thirdNum is : " + thirdNum);
+
+        // Processing
+        // Compare the inputs with nested if statements.
+
+        int largestOfThree;
+
+        if (firstNum > secondNum)
+        {
+
+        }
+            if (firstNum > thirdNum)
+            {
+
+            }
+                largestOfThree = firstNum;
+
+            else largestOfThree = thirdNum;
+
+        if (secondNum > thirdNum);
+        {
+
+        }
+
+
+            else
+
+        {
+
+        }
+
+                largestOfThree = secondNum;
+                        else
+                            largestOfThree = thirdNum;
+
+                        // Output largest of three
+    System.out.println(("\n The largest of three is: " + largestOfThree);
+
+
+
+
+
+
